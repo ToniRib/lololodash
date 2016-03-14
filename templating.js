9779cda67d0b1a13a99bf994740665776d902fe8
ToniRib <tonimarierib@gmail.com>
2016-03-14 14:56:45 -0600

    Add templating solution

---
+++ templating.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _ = require("lodash");
+
+var template = function(user) {
+    var compiled = _.template('Hello <%= user.name %> (logins: <%= user.login.length %>)');
+    return compiled({ user: user });
+};
+
+module.exports = template;
