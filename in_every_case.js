0d8030f182b51c0af6f8f4727ff3718b24203aa1
ToniRib <tonimarierib@gmail.com>
2016-03-16 16:36:52 -0600

    Rename for_each to in_every_case

--- in_every_case.js
+++ in_every_case.js
@@ -5,11 +5,11 @@ var _ = require("lodash");
 var addSize = function(cities) {
   return _.forEach(cities, function(city) {
     if (city.population > 1.0) {
-      return city.size = 'big';
+      city.size = 'big';
     } else if (city.population > 0.5) {
-      return city.size = 'med';
+      city.size = 'med';
     } else {
-      return city.size = 'small';
+      city.size = 'small';
     }
   })
 }
